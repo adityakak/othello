6b5e3b58fac6d1899e14cfddf2aaedc348f97d48
Adi <adityakak04@gmail.com>
2023-04-24 03:12:27 -0400

    update file

--- othellogui/src/containers/index.tsx
+++ othellogui/src/containers/index.tsx
@@ -1,4 +1,4 @@
-export { default as Board_Fatures} from './board_features/Board-Features';
+export { default as Board_Features} from './board_features/Board_Features';
 export { default as Footer} from './footer/Footer';
 export { default as Information} from './information/Information';
 export { default as Header} from './header/Header';
