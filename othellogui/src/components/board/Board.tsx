18850b3376e33c2feeccb72d61d93dbc6e4937a6
Adi <adityakak04@gmail.com>
2023-05-06 04:26:38 -0400

    Adding to board

--- othellogui/src/components/board/Board.tsx
+++ othellogui/src/components/board/Board.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './board.css';
 import { Square } from '../index';
 
 const Board = () => {
+  const initialArray: number[][] = Array.from({ length: 8 }, () => Array(8).fill(0));
+  initialArray[3][3] = 1;
+  initialArray[3][4] = 2;
+  initialArray[4][3] = 2;
+  initialArray[4][4] = 1;
+
+  const [boardState, setBoardState] = useState<number[][]>(initialArray);
+  const [isClickable, setIsClickable] = useState<boolean>(true);
+
+  const handleSquareClick = (row: number, col: number) => {
+    if (!isClickable) return; 
+    const updatedBoardState = boardState.map((rowArr, rowIndex) =>
+      rowIndex === row ? rowArr.map((value, colIndex) => (colIndex === col ? 2 : value)) : rowArr
+    );
+    setBoardState(updatedBoardState);
+    setIsClickable(false); 
+    setTimeout(() => {
+      setIsClickable(true);
+    }, 3000);
+  };
+
   const renderSquares = () => {
     const squares = [];
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
         let squareId = `${row}-${col}`;
-        squares.push(<Square key={squareId} state={0} />);
+        squares.push(<Square key={squareId} value={squareId} state={boardState[row][col]} onClick={() => handleSquareClick(row, col)} />);
       }
     }
     return squares;
--- othellogui/src/components/square/Square.tsx
+++ othellogui/src/components/square/Square.tsx
@@ -5,17 +5,22 @@ import blackPiece from '../../assets/blackPiece.png';
 
 interface SquareProps {
     key: string;
+    value: string;
     state: number; // 0 = empty, 1 = white, 2 = black
+    onClick: () => void;
   }
 
 const Square: React.FC<SquareProps> = props => {
+    const handleClick = () => {
+        props.onClick && props.onClick();
+    };
     if(props.state === 0) {
-        return <button className="othello_square" id={props.key}>  </button>;
+        return <button className="othello_square" id={props.value} onClick={handleClick}>  </button>;
     } else if(props.state === 1) {
-        return <button className="othello_square" id={props.key}> <img src={whitePiece} alt="White" /> </button>;
+        return <button className="othello_square" id={props.value}> <img src={whitePiece} alt="White" /> </button>;
     }
     else{
-        return <button className="othello_square" id={props.key}> <div className="othello_black"> <img src={blackPiece} alt="Black" /> </div> </button>;
+        return <button className="othello_square" id={props.value}> <div className="othello_black"> <img src={blackPiece} alt="Black" /> </div> </button>;
     }
 };
 
