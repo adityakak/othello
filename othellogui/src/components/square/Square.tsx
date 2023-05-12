65696c4e4210d69c4f419f34ac49ed0e00498639
Adi <adityakak04@gmail.com>
2023-05-12 04:56:37 -0400

    linting

--- othellogui/src/components/square/Square.tsx
+++ othellogui/src/components/square/Square.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import './square.css';
-import whitePiece from '../../assets/whitePiece.png';
-import blackPiece from '../../assets/blackPiece.png';
+import React from 'react'
+import './square.css'
+import whitePiece from '../../assets/whitePiece.png'
+import blackPiece from '../../assets/blackPiece.png'
 
 interface SquareProps {
-    key: string;
-    value: string;
-    state: number; // 0 = empty, 1 = white, 2 = black
-    onClick: () => void;
-  }
+  key: string
+  value: string
+  state: number// 0 = empty, 1 = white, 2 = black
+  onClick: () => void
+}
 
 const Square: React.FC<SquareProps> = props => {
-    const handleClick = () => {
-        props.onClick && props.onClick();
-    };
-    if(props.state === 0) {
-        return <button className="othello_square" id={props.value} onClick={handleClick}>  </button>;
-    } else if(props.state === 1) {
-        return <button className="othello_square" id={props.value}> <img src={whitePiece} alt="White" /> </button>;
-    }
-    else{
-        return <button className="othello_square" id={props.value}> <div className="othello_black"> <img src={blackPiece} alt="Black" /> </div> </button>;
-    }
-};
+  const handleClick = (): void => {
+    props.onClick?.()
+  }
+  if (props.state === 0) {
+    return <button className="othello_square" id={props.value} onClick={handleClick}>  </button>
+  } else if (props.state === 1) {
+    return <button className="othello_square" id={props.value}> <img src={whitePiece} alt="White" /> </button>
+  } else {
+    return <button className="othello_square" id={props.value}> <div className="othello_black"> <img src={blackPiece} alt="Black" /> </div> </button>
+  }
+}
 
-export default Square;
+export default Square
