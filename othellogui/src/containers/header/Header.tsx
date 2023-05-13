c9cc9efdcbf8f0540a9e42fa5be8f6ca1f9c93a6
Adi <adityakak04@gmail.com>
2023-05-13 02:59:05 -0400

    vw instead of rem and px

--- othellogui/src/containers/header/Header.tsx
+++ othellogui/src/containers/header/Header.tsx
@@ -5,7 +5,6 @@ import headerImage from '../../assets/headerImage.png'
 const Header: React.FC = (): JSX.Element => {
   return (
     <div className="othello_header_padding">
-      <div className="othello_header">
         <div className="othello_headline" id="explain">
           <h1> OthelloAI </h1>
         </div>
@@ -14,7 +13,6 @@ const Header: React.FC = (): JSX.Element => {
           <p> Welcome to OthelloAI! Here, you have the opportunity to challenge two types of artificial intelligence, putting your strategic abilities to the test against a machine. We offer two distinct AI implementations: one model that employs a combination of minimax algorithm, tuned heuristics, and alpha-beta pruning, and another model that utilizes PyTorch, reinforcement learning, neural networks, and OpenAI Gym.
               Playing against these two AI models can provide you with a unique opportunity to learn and improve your gameplay, discovering what ideas the AI prioritizes.
           </p>
-        </div>
       </div>
     </div>
   )
